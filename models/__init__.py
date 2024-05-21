6a452aa6c9a78f2489bc6f592db3dc7bddd008a6
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-05-21 06:22:09 +0200

    fixing base_model

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,1 @@
 #!/usr/bin/env python3
-from models import storage
-
-
-storage = FileStorage()
-storage.reload()
--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""
+Init for model models
+"""
+import models
+
+models.engine.storage = FileStorage()
+modles.engine.storage.reload()
